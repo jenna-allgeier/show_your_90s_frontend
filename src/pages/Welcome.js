7aae997c0996b930f187e57860e7528fbb50dcab
Aqsa Saleheene <angieclrk21@gmail.com>
2022-04-20 14:57:36 -0400

    CSS

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
-    <div className="welcome">
-      <div className="welcome-text">
-        <h1 className="show-your-90s">Show Your 90's!</h1>
-        <h1 className="welcomeMessage">Finally! A place to share your favorite 90s memorabilia!</h1>
-      </div>
+    <div className="welcome backgroundimg">
+      <img className="carimg" src="https://d63oxfkn1m8sf.cloudfront.net/5416/1113/7724/c4249_3_bttf2-delorean_product.jpg" />
+      {/* <div className="welcome-text">
+        <h1 className="show-your-90s"></h1>
+        <h1 className="welcomeMessage"></h1>
+      </div> */}
       <Link to='/login'>
       <button className="welcome-btns" type="submit">Login</button>
-      <button className="welcome-btns" type="submit">Register</button>
+      <button className="register-btn" type="submit">Register</button>
       </Link>
     </div>
   );
 };
 
-export default Welcome;
+export default Welcome 
